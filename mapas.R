e37d062a649449411772ba563aeddecf406b6606
pepp47 <86166407+pepp47@users.noreply.github.com>
2022-06-17 18:52:00 -0500

    Mapa de carreteras
    Carreteras federales en México sin cuota

---
+++ mapas.R
@@ -0,0 +1,40 @@
+# mapa de carreteras federales --------------------------------------------
+
+
+library(tidyverse)
+library(rgdal)
+library(sf)
+library(RColorBrewe)
+library(ggthemes)
+
+
+
+map_est<-read_sf("gdb_ref_esta_ine_2009.shp")
+map_vial<-read_sf("red_vial.shp")
+
+# Filtros para el mapa Federal y sin peaje
+
+map_vial%>%
+  filter(JURISDI== "Fed." & PEAJE == "No" )%>%
+  arrange(LONGITUD)->map_vial_1
+  
+
+map_vial_1$RECUBRI<-factor(map_vial_1$RECUBRI)
+
+ ggplot(map_est)+
+   geom_sf()+
+   theme_bw()+
+   xlab("Longitud") + ylab("Latitud")+
+   geom_sf(data=map_vial_1, aes(color = RECUBRI))+
+   scale_color_brewer(name="Recubrimiento\nde las\ncarreteras",
+                      palette = "Dark2")+
+   labs(title = "Carreteras federales, con y sin peaje, 2021")->mapa_ca
+
+ggsave(mapa_ca, "mapa_ca.png", dpi = 500 )
+
+
+
+
+
+dir()
+
